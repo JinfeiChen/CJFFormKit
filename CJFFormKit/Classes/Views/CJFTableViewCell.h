0e852e95aec820f203901eee79124cb1dc27bcf6
JinfeiChen <jinfei_chen@icloud.com>
2020-12-01 17:53:31 +0800

    update

--- CJFFormKit/Classes/Views/CJFTableViewCell.h
+++ CJFFormKit/Classes/Views/CJFTableViewCell.h
@@ -12,11 +12,13 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CJFTableViewCellStyle : NSObject
 
+@property (assign, nonatomic) UIEdgeInsets contentInset; /**< <#property#> */
+
 @end
 
 @interface CJFTableViewCell : UITableViewCell
 
-//@property (strong, nonatomic) CJFTableViewCellStyle *style; /**< <#property#> */
+@property (strong, nonatomic) CJFTableViewCellStyle *style; /**< <#property#> */
 
 - (void)setModelWithDict:(NSDictionary * _Nullable)dict format:(NSDictionary * _Nullable)format;
 
