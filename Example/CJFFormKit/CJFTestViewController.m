aacdc15444d9d0dfa7140e1f8b1a7f2c9cc75897
JinfeiChen <jinfei_chen@icloud.com>
2020-12-01 17:46:04 +0800

    update

--- Example/CJFFormKit/CJFTestViewController.m
+++ Example/CJFFormKit/CJFTestViewController.m
@@ -20,7 +20,7 @@
 
 #import <CJFFormKit/testView.h>
 #import <CJFFormKit/subTestView.h>
-#import <CJFFormKit/CJFSubTableViewCell.h>
+//#import <CJFFormKit/CJFSubTableViewCell.h>
 #import <CJFFormKit/CJFFormLR002TableViewCell.h>
 
 @interface CJFTestViewController ()
