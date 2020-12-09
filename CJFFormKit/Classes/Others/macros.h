b07085c17024a9579c3bcacb5c5168a811cf2b17
JinfeiChen <jinfei_chen@icloud.com>
2020-12-09 14:22:01 +0800

    update

--- CJFFormKit/Classes/Others/macros.h
+++ CJFFormKit/Classes/Others/macros.h
@@ -8,6 +8,8 @@
 #ifndef macros_h
 #define macros_h
 
+#define kCJFFormResourceBundle [NSBundle bundleWithPath:[[NSBundle bundleForClass:[self class]].resourcePath stringByAppendingPathComponent:@"/CJFFormKit.bundle"]]
+
 #define kCJFFormHexColor(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 #define kCJFFormMainColor [UIColor colorWithRed:255/255.0 green:194/255.0 blue:76/255.0 alpha:1.0]
